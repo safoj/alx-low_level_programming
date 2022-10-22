864caf91e1c1c91bb6f83c6968f4abb338afc93d
Safo Kofi <safojuniork@gmail.com>
2022-10-21 19:23:00 -0700

    prints triangle

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - print fizzbuzz
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	printf("1");
+	for (n = 2; n <= 100; n++)
+	{
+		printf(" ");
+		if (n % 3 == 0)
+			printf("Fizz");
+		if (n % 5 == 0)
+			printf("Buzz");
+		if (n % 3 != 0 && n % 5 != 0)
+			printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
