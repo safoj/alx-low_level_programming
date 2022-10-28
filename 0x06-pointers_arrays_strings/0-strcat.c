85b34e8985f8e1e2b8f3a9e88921e09753cafc1b
Safo Kofi <safojuniork@gmail.com>
2022-10-28 10:26:00 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,19 @@
 
 char *strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] - src[index];
-
+	int index = 0, b;
+	
+	while (dest[x])
+	{
+		x++;
+	}
+	
+	for (b = 0; src[b] != 0; b++)
+	{
+		dest[x] = src[b];
+		x++;
+	}
+	
+	dest[x] = '\0';
 	return (dest);
 }
