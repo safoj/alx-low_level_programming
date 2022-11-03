9cf5f6a058514253b6104a25055828b002d4b0c6
Safo Kofi Junior <111443099+safoj@users.noreply.github.com>
2022-11-03 15:22:16 +0000

    returns the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -25,15 +25,14 @@ int _evaluate(int i, int n)
  *  *_sqrt_recursion - evaluate sqrt
  *  *@n: integer
  *  Return: sqrt_recursion
- * */
+ */
 
 int _sqrt_recursion(int n)
 {
 	int i = 0;
-	
+
 	if (i < 0)
 		return (-1);
-	else
 	{
 		return (_evaluate(i, n));
 	}
