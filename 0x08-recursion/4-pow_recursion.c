7c62266ac95bf62b1ea91828eb473eb2d67c30ea
Safo Kofi Junior <111443099+safoj@users.noreply.github.com>
2022-11-03 14:57:46 +0000

    value of x raised to the power of y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * factorial - factorial of a given number
- * @n: pointer block of memory to fill
- * Return: factorial
+ * _pow_recursion - returns value of x raised to the power of y
+ * @x: integer
+ * @y: integer
+ * Return: pow recursion
  */
 
-int factorial(int n)
+int _pow_recursion(int x, int y)
 {
-	if (n == 0)
-		return (1);
-	else if (n < 0)
+	if (y < 0)
+	{
 		return (-1);
+	}
+	else if (y != 0)
+		return (x * _pow_recursion(x, y - 1));
 	else
-		return (n * factorial(n - 1));
-
+		return (1);
 }
