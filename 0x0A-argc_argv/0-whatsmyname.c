6fe34826ae65d98f6b57683c63296e4e578a50c5
Safo Kofi Junior <111443099+safoj@users.noreply.github.com>
2022-11-05 12:19:04 +0000

    Prints the programs name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,18 @@
-#include <stdio.h>                                                  
-#include "main.h"                                                   
-                                                                    
-/**                                                                 
- * main - Print the name of the program                             
- * @argc: Count arguments                                           
- * @argv: arguments                                                 
- *                                                                  
- * Return: always 0 (success)                                       
- */                                                                 
-                                                                    
-int main(int argc, char *argv[])                                    
-{                                                                   
-        (void)argc;                                                 
-        printf("%s\n", argv[0]);                                    
-                                                                    
-        return (0);                                                 
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: arguments
+ *
+ * Return: always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+        (void)argc;
+        printf("%s\n", argv[0]);
+
+        return (0);
 }
