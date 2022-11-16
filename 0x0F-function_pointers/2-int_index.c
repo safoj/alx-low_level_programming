35d02b7263bf1036680312dfd7ee7ed2819c7e83
Safo Kofi Junior <111443099+safoj@users.noreply.github.com>
2022-11-16 18:21:30 +0000

    searches for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,11 +17,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array && cmp)
 	{
-		if (size <= 0)
-			return (-1);
+		//if (size <= 0)
+		//	return (-1);
 		for (i = 0; i < size; i++)
+		{
 			if (cmp(array[i]) != 0)
 				return (i);
+		}
 	}
-	return (-1);
+	//return (-1);
 }
